9cc8635dcfe616f96fc8edfb40a0558cbf3c4bfe
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-17 09:14:00 +0400

    feat: Add database table and API endpoints for managing messages

--- src/controllers/contactRoutes.js
+++ src/controllers/contactRoutes.js
@@ -1,3 +1,4 @@
+import prisma from "../config/database.js";
 import { transporter } from "../utils/contactAdmin.js";
 
 export const contactAdmin = async (req, res) => {
@@ -6,6 +7,14 @@ export const contactAdmin = async (req, res) => {
   if (!name || !email || !message) {
     return res.status(400).json({ message: "All fields are required" });
   }
+
+  await prisma.message.create({
+    data: {
+      name,
+      email,
+      message,
+    },
+  });
 
   const mailOptions = {
     from: email,
@@ -20,5 +29,31 @@ export const contactAdmin = async (req, res) => {
   } catch (error) {
     console.error("Error sending email:", error);
     res.status(500).json({ message: "Error sending email" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const messages = await prisma.message.findMany();
+
+    res.status(200).json(messages);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const deleteMessageById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await prisma.message.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    res.status(200).json({ message: "Message deleted" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
 };
--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { contactAdmin } from "../controllers/contactRoutes.js";
+import {
+  contactAdmin,
+  deleteMessageById,
+  getMessages,
+} from "../controllers/contactRoutes.js";
 
 const router = express.Router();
 
 router.post("/contact", contactAdmin);
+
+router.get("/messages", getMessages);
+
+router.delete("/delete_message/:id", deleteMessageById);
 
 export default router;
