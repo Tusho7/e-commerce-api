bd306122b0b89829e560692fad176f0c9dbaa3a3
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-19 15:08:35 +0400

    feat: Add CRUD functionality for contacts

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
 import {
   contactAdmin,
+  createContact,
   deleteMessageById,
+  editContactById,
+  getContacts,
   getMessages,
 } from "../controllers/contactRoutes.js";
 
@@ -12,5 +15,11 @@ router.post("/contact", contactAdmin);
 router.get("/messages", getMessages);
 
 router.delete("/delete_message/:id", deleteMessageById);
+
+router.post("/create_contact_us", createContact);
+
+router.post("/edit_contact_us/:id", editContactById);
+
+router.get("/get_contact_us", getContacts);
 
 export default router;
