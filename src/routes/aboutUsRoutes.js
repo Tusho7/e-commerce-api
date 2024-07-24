6487b85b0d169cef1fea98869561490a1dc94acc
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-24 16:19:28 +0400

    chore: change endpoint for update about us

--- src/routes/aboutUsRoutes.js
+++ src/routes/aboutUsRoutes.js
@@ -3,6 +3,7 @@ import {
   getAboutUs,
   createAboutUs,
   deleteAboutUsById,
+  editAboutUsById,
 } from "../controllers/aboutUsController.js";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/get_aboutus", getAboutUs);
 router.post("/add_aboutus", createAboutUs);
 router.delete("/delete_aboutus/:id", deleteAboutUsById);
+router.put("/update_aboutus/:id", editAboutUsById);
 
 export default router;
