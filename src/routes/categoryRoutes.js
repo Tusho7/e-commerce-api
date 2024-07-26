a2f9751bdeddbd20f70ab3c93576037abb166aa6
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-26 15:04:43 +0400

    fix: route in category

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -5,9 +5,6 @@ const router = express.Router();
 router.post("/create_category", categoryController.createCategory);
 router.get("/get_categories", categoryController.getCategories);
 router.delete("/delete_category/:id", categoryController.deleteCategoryById);
-router.patch(
-  "/update_category_name/:id",
-  categoryController.updateCategoryName
-);
+router.put("/update_category_name/:id", categoryController.updateCategoryName);
 
 export default router;
