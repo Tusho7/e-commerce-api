67185ddc292623ee25a0330c489bbf436c9175ab
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-26 09:37:10 +0400

    feat: add rljte flr edit career

--- src/routes/careerRoutes.js
+++ src/routes/careerRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createCareers,
   deleteCareerById,
+  editCareerById,
   getCareers,
 } from "../controllers/careersController.js";
 
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/get_careers", getCareers);
 router.post("/add_career", createCareers);
 router.delete("/delete_career/:id", deleteCareerById);
+router.put("/edit_career/:id", editCareerById);
 
 export default router;
